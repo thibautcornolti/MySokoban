964ab1ba800f36447bf90b67d5b69aca9073d1cd
Thibaut Cornolti <thibaut.cornolti@epitech.eu>
2016-12-16 19:35:35 +0100

    Loose handler

--- check_game.c
+++ check_game.c
@@ -5,19 +5,45 @@
 ** Login   <thibaut.cornolti@epitech.eu>
 ** 
 ** Started on  Sun Dec 11 18:18:07 2016 Thibaut Cornolti
-** Last update Fri Dec 16 19:26:21 2016 Thibaut Cornolti
+** Last update Tue Dec 13 13:20:07 2016 Thibaut Cornolti
 */
 
 #include "soko.h"
+
+static int	check_box(t_game *g, int x, int y)
+{
+  int		mv;
+
+  mv = 0;
+  if ((g->map[y + 1][x] == '#' || pos_is_box(g, x, y + 1)) &&
+      (g->map[y][x + 1] == '#' || pos_is_box(g, x + 1, y)))
+    mv += 1;
+  if ((g->map[y - 1][x] == '#' || pos_is_box(g, x, y - 1)) &&
+      (g->map[y][x + 1] == '#' || pos_is_box(g, x + 1, y)))
+    mv += 1;
+  if ((g->map[y + 1][x] == '#' || pos_is_box(g, x, y + 1)) &&
+      (g->map[y][x - 1] == '#' || pos_is_box(g, x - 1, y)))
+    mv += 1;
+  if ((g->map[y - 1][x] == '#' || pos_is_box(g, x, y - 1)) &&
+      (g->map[y][x - 1] == '#' || pos_is_box(g, x - 1, y)))
+    mv += 1;
+  if (mv >= 1)
+    return (1);
+  return (0);
+}
 
 int		check_game(t_game *g)
 {
   int		i;
   int		j;
+  int		kobox;
   int		ko;
+  int		box;
 
   i = -1;
   ko = 0;
+  kobox = 0;
+  box = 0;
   while (g->map[++i])
     {
       j = -1;
@@ -25,9 +51,13 @@ int		check_game(t_game *g)
 	if (g->map[i][j] == 'O' &&
 	    !pos_is_box(g, j, i))
 	  ko += 1;
+	else if (pos_is_box(g, j, i))
+	  kobox += check_box(g, j, i) + 0 * (box += 1);
     }
   if (ko == 0)
     stop_game_r(0);
-    return (1);
+  if (kobox == box)
+    stop_game_r(1);
+  return (1);
 }
 
